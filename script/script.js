aeda5b25cdd98f49150229678bca9097deafa74c
Chabanov Sergay <chabanovsn@mail.ru>
2018-12-28 18:06:08 +0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,26 @@
+
+   function gem(){ 
+    var show =  document.getElementById("show"); 
+   show.innerHTML= "<a href='lab/gem.html'> \
+   <h2 align='center'> Гематологический анализатор</h2> \
+   <img src = images/mindray.jpg id ='imgid'></a>";   
+
+   }
+   function bio(){ 
+    var show =  document.getElementById("show"); 
+   show.innerHTML= "<a href='lab/bio.html'> \
+   <h2 align='center'> Биохимический анализатор</h2> \
+   <img src = images/bio600.jpg id ='imgid'></a>";   
+
+   }
+   function immy(){ 
+    var show =  document.getElementById("show"); 
+   show.innerHTML= "<a href='lab/immy.html'> \
+   <h2 align='center'> Иммуноферментный анализатор</h2> \
+   <img src = images/immy.jpg id ='imgid'></a>";  
+
+   }
+
+
+
+    
